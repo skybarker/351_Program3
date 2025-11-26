296265f3d63fa18c171e910405d18b0f2a591a07
skybarker <luke.s.barker@gmail.com>
2025-11-26 20:53:47 +0100

    ExplodingCrates

--- Program3/Assets/Scripts/Explode.cs
+++ Program3/Assets/Scripts/Explode.cs
@@ -8,14 +8,22 @@ public class Explode : MonoBehaviour
    [SerializeField]private GameObject prefab;
    [SerializeField]private AudioSource Boom;
 
+    void Start()
+    {
+        explode = transform.Find("BigExplosion").GetComponent<ParticleSystem>();    
+        Boom = GetComponent<AudioSource>();
+        Renderer renderer = GetComponent<Renderer>();   
+    }
+
 
     private void OnCollisionEnter(Collision other)
     {
-        // if (other.gameObject.tag == "Bullet"){
-        //     //explode.Play();
-        //     //Boom.Play();
-        //     //GameObject brokenBarrel = Instantiate(prefab, transform.position, transform.rotation);
-        //     //Destroy(gameObject);
-        //     }
+        if (other.gameObject.tag == "Bullet"){
+            explode.Play(true);
+            Boom.Play();
+            GameObject brokenBarrel = Instantiate(prefab, transform.position, transform.rotation);
+            GetComponent<Renderer>().enabled = false;
+            //Destroy(gameObject);
+            }
     }
 }
